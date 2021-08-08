a78576a2a1a24d1f8bf692759b9f07b0962a6663
ArseniyGrinch <arseniygrinch@gmail.com>
2021-08-08 15:29:52 +0500

    add contacts widthout map

---
+++ #src/js/scripts-contacts.js
@@ -0,0 +1,13 @@
+$(function(){
+   /* header menu */
+
+   $('.menu').click(function(){
+      $(this).toggleClass('menu--active');
+      $('.header__inner').toggleClass('header__inner--active');
+   })
+
+   /* -- header menu end -- */
+
+   /* scroll mouse */
+   
+});
