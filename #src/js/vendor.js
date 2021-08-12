a6df22d98aab99a09deb59bdaecc84787bb6785a
pustoy <pustoy.dev@gmail.com>
2021-08-12 19:06:23 +0300

    заготовил поддержку webp в сss

--- #src/js/vendor.js
+++ #src/js/vendor.js
@@ -1,3 +1,4 @@
+require('./vendor/modernizr-webp.js');
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
@@ -5,11 +6,11 @@ import GLightbox from 'glightbox';
 require('../../node_modules/slick-carousel/slick/slick.min.js')
 
 //-- этот скрипт загружает модуль для тестирования, его не должно быть в финальном билде и на проде для клиента
-import {testInstruments} from './vendor/testInstruments';
+// import {testInstruments} from './vendor/testInstruments';
 
-window.addEventListener('load', () => {
-  testInstruments();
-});
+// window.addEventListener('load', () => {
+//   testInstruments();
+// });
 //--
 
 const lightbox = GLightbox({
