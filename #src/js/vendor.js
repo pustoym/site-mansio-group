e452179fbb1d7d2e3167ea49fcb8b8e14f9da366
pustoy <pustoy.dev@gmail.com>
2021-08-20 14:54:40 +0300

    passive listeners JQuery fix

--- #src/js/vendor.js
+++ #src/js/vendor.js
@@ -2,6 +2,29 @@ require('./vendor/modernizr-webp.js');
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
+// lighthouse fix  Does not use passive listeners to improve scrolling performance
+// more https://stackoverflow.com/questions/60357083/does-not-use-passive-listeners-to-improve-scrolling-performance-lighthouse-repo
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.wheel = {
+  setup: function( _, ns, handle ){
+      this.addEventListener("wheel", handle, { passive: true });
+  }
+};
+jQuery.event.special.mousewheel = {
+  setup: function( _, ns, handle ){
+      this.addEventListener("mousewheel", handle, { passive: true });
+  }
+};
+
 import GLightbox from 'glightbox';
 require('../../node_modules/slick-carousel/slick/slick.min.js')
 
